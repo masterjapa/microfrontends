b6b44ec1f2722f34790dce59e241ae1493f931a3
Rollback CI <runner@fv-az123-322.ta1s1eqq5bxetg4z15lvlszd4f.phxx.internal.cloudapp.net>
2021-12-01 13:23:07 +0000

    Revert "Merge pull request #130 from masterjapa/rollback/128"
    This reverts commit 32de1b39a4f3d0ad968a68c275f82eaf6d5f02b8, reversing
    changes made to f8b5eeb1bcb2c6383aac598545682ad6f8b848c4.

--- packages/shell/src/App.vue
+++ packages/shell/src/App.vue
@@ -59,9 +59,8 @@ export default {
         await this.fetchReactLoader();
         await this.fetchReactDomLoader();
         await this.fetchReactComponent();
+        console.info("elemento montado");
       } catch (error) {
-        console.error("erro no mount", error);
-        console.error("erro no mount", error);
         console.error("erro no mount", error);
       }
 
